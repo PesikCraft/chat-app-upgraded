bf19da05ef737f33c91ffd4bd8ce4367f632aad8
PesikCraft <72542219+PesikCraft@users.noreply.github.com>
2025-02-24 11:09:51 +0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const path = require("path");
+const useragent = require("useragent");
+const os = require("os");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const users = {};
+const bannedUsers = new Set();
+
+io.on("connection", (socket) => {
+    // Получение информации об устройстве
+    const agent = useragent.parse(socket.handshake.headers["user-agent"]);
+    const deviceInfo = `${agent.family} ${agent.major} (OS: ${os.type()} ${os.release()})`;
+
+    // Получение IP-адреса
+    let userIP = socket.handshake.address;
+    if (userIP === "::1" || userIP === "127.0.0.1") {
+        userIP = "Локальный IP (Тест)";
+    } else {
+        userIP = socket.request.connection.remoteAddress;
+    }
+
+    socket.on("setUsername", (username) => {
+        if (bannedUsers.has(username)) {
+            socket.emit("banned");
+            return;
+        }
+
+        console.log(`📌 Новый пользователь: ${username}`);
+        console.log(`📱 Устройство: ${deviceInfo}`);
+        console.log(`🌍 IP-адрес: ${userIP}`);
+
+        users[socket.id] = username;
+        io.emit("chatMessage", { username: "Система", message: `👤 ${username} присоединился к чату` });
+    });
+
+    // Обработчик сообщений
+    socket.on("chatMessage", (data) => {
+        io.emit("chatMessage", data);
+    });
+
+    // Очистка чата
+    socket.on("clearChat", (admin) => {
+        io.emit("clearChat", admin);
+    });
+
+    // Кик пользователя
+    socket.on("kickUser", (data) => {
+        const { userToKick, admin } = data;
+        const kickedSocketId = Object.keys(users).find((id) => users[id] === userToKick);
+        if (kickedSocketId) {
+            io.to(kickedSocketId).emit("kicked");
+            io.emit("chatMessage", { username: "Система", message: `🚪 ${userToKick} был кикнут администратором ${admin}` });
+            io.sockets.sockets.get(kickedSocketId).disconnect();
+        }
+    });
+
+    // Бан пользователя
+    socket.on("banUser", (data) => {
+        const { userToBan, admin } = data;
+        bannedUsers.add(userToBan);
+        const bannedSocketId = Object.keys(users).find((id) => users[id] === userToBan);
+        if (bannedSocketId) {
+            io.to(bannedSocketId).emit("banned");
+            io.emit("chatMessage", { username: "Система", message: `⛔ ${userToBan} был забанен администратором ${admin}` });
+            io.sockets.sockets.get(bannedSocketId).disconnect();
+        }
+    });
+
+    // Разбан пользователя
+    socket.on("unbanUser", (userToUnban) => {
+        bannedUsers.delete(userToUnban);
+        io.emit("chatMessage", { username: "Система", message: `🔓 ${userToUnban} был разбанен` });
+    });
+
+    // Обработка ввода (кто печатает)
+    socket.on("typing", (username) => {
+        socket.broadcast.emit("displayTyping", username);
+    });
+
+    socket.on("stopTyping", () => {
+        socket.broadcast.emit("hideTyping");
+    });
+
+    // Отключение пользователя
+    socket.on("disconnect", () => {
+        if (users[socket.id]) {
+            io.emit("chatMessage", { username: "Система", message: `🚪 ${users[socket.id]} покинул чат` });
+        }
+        delete users[socket.id];
+    });
+});
+
+server.listen(3000, () => {
+    console.log("🚀 Сервер запущен на порту 3000");
+});
